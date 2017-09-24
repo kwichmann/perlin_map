146a60c83ba5c30c03972d832fe39067b24fc4af
kwichmann <kristian.wichmann@gmail.com>
2017-09-25 00:18:29 +0200

    Streamlining of map generation/drawing

--- docs/map.js
+++ docs/map.js
@@ -1,21 +1,25 @@
 const granularity = 5;
-let e_seed = 42;
-let v_seed = 113;
+let e_seed = 71942;
+let v_seed = 1713;
 const noiseMesh = 1 / 100;
-let elevation, vegetation;
+
+let col_map;
 
 function setup() {
   createCanvas(600, 400);
-  make_map();
+  col_map = make_map(e_seed, v_seed, 0.25, 0.3, 0.33, 0.65, 0.25, 0.75);
+  console.log(col_map);
 }
 
 function draw() {
-    draw_map();
+    draw_map(col_map);
 }
 
-function make_map() {
-  noiseSeed(e_seed);
-  elevation = [];
+function make_map(elevation_seed, vegetation_seed, deep_sea, sea, swamp, grass, forest, mountains) {
+
+  // Make elevation map
+  noiseSeed(elevation_seed);
+  let elevation = [];
 
   for (let y = 0; y < height; y += granularity) {
     let row = [];
@@ -25,7 +29,8 @@ function make_map() {
     elevation.push(row);
   }
 
-  noiseSeed(v_seed);
+  // Make vegetation map
+  noiseSeed(vegetation_seed);
   vegetation = [];
 
   for (let y = 0; y < height; y += granularity) {
@@ -35,34 +40,72 @@ function make_map() {
     }
     vegetation.push(row);
   }
-}
 
-function draw_map() {
-  noStroke();
+  // Turn into actual terrain map
+  let terrain_map = [];
 
   for (let i = 0; i < elevation.length; i++) {
+    let row = [];
     for (let j = 0; j < elevation[i].length; j++) {
       let terrain = elevation[i][j];
-      let flora = vegetation[i][j]
+      let flora = terrain * vegetation[i][j]
 
-      let c = map(terrain, 0, 0.3, 0, 100);
-      fill(c, c, 255);
-
-      if (terrain > 0.3) {
-        fill(200, 200, 100);
+      // Check for deep sea
+      if (terrain <= deep_sea) {
+        row.push(0);
       }
-      if (terrain > 0.33) {
-        fill(0, 255, 0);
-        if (flora * terrain > 0.25) {
-          fill(0, 180, 0);
+
+      // Check for sea
+      if (terrain > deep_sea && terrain <= sea) {
+        row.push(1);
+      }
+
+      // Check for swamp
+      if (terrain > sea && terrain <= swamp) {
+        row.push(2);
+      }
+
+      // Check for grasslands
+      if (terrain > swamp && terrain <= grass) {
+        // ... unless it's a forest
+        if (flora > forest) {
+          row.push(4);
+        } else {
+          row.push(3);
         }
       }
-      if (terrain > 0.65) {
-        c = map(terrain, 0.65, 1, 180, 255);
-        fill(c);
-      }
-      rect(j * granularity, i * granularity, granularity, granularity);
 
+      // Check for mountains
+      if (terrain > grass && terrain <= mountains) {
+        row.push(5);
+      }
+
+      // Check for snow
+      if (terrain > mountains) {
+        row.push(6);
+      }
+    }
+    terrain_map.push(row);
+  }
+  return terrain_map;
+}
+
+function draw_map(terrain_map) {
+  noStroke();
+  const map_colors = [
+    color(0, 0, 255),       // Color of deep sea
+    color(100, 100, 255),   // Color of sea
+    color(200, 200, 100),   // Color of swamp
+    color(0, 255, 0),       // Color of grasslands
+    color(0, 180, 0),       // Color of forests
+    color(180, 180, 180),   // Color or mountains
+    color(255, 255, 255)    // Color of snow
+  ];
+
+  for (let i = 0; i < terrain_map.length; i++) {
+    for (let j = 0; j < terrain_map[i].length; j++) {
+      fill(map_colors[terrain_map[i][j]]);
+      rect(j * granularity, i * granularity, granularity, granularity);
     }
   }
 }
